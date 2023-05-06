36ac4cd94b4ff8dc98b2fff53ecbca8fb0126591
BernardoModerno <bernardo.moderno@gmail.com>
2023-05-06 19:20:51 -0300

    feat: insert axios and first resquest

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+import Button from '../../../shared/buttons/button/Button';
 import Input from '../../../shared/inputs/input/Input';
 import {
   BackgroundImage,
@@ -5,16 +9,58 @@ import {
   ContainerLoginScreen,
   LimitedContainer,
   LogoImage,
+  TitleLogin,
 } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async () => {
+    await axios({
+      method: 'post',
+      url: 'http://localhost:8080/auth',
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+      .then((result) => {
+        alert(`Fez login ${result.data.accessToken}`);
+        return result.data;
+      })
+      .catch(() => {
+        alert('Usuário ou senha inválido');
+      });
+  };
+
   return (
     <ContainerLoginScreen>
       <ContainerLogin>
         <LimitedContainer>
           <LogoImage src="./logo.png" />
-          <Input title="USUÁRIO" />
-          <Input title="SENHA" />
+          <TitleLogin level={2} type="secondary">
+            LOGIN
+          </TitleLogin>
+          <Input title="USUÁRIO" margin="32px 0px 0px" onChange={handleEmail} value={email} />
+          <Input
+            type="password"
+            title="SENHA"
+            margin="32px 0px 0px"
+            onChange={handlePassword}
+            value={password}
+          />
+          <Button type="primary" margin="64px 0px 16px 0px" onClick={handleLogin}>
+            ENTRAR
+          </Button>
         </LimitedContainer>
       </ContainerLogin>
       <BackgroundImage src="./background.png" />
